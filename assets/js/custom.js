e6f4ba1ef3d5dd7060d11a10afbb20942cd038fd
AndersonHqds <andyhhenrikefs@gmail.com>
2018-10-31 20:33:33 -0300

    Nova atualização

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event){
-	//alert(screen.height);
 	const headerSize = document.getElementById('header').clientHeight;
-	alert(headerSize);
 	document.getElementById('container-background').style.height = (window.innerHeight - headerSize) + "px";
 });
